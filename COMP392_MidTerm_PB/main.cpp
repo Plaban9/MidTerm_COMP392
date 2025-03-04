c20162127b7be5612501ff80fe0a1c03490e502a
Plaban Biswas <plabanbiswas96@outlook.com>
2025-03-03 19:43:38 -0500

    - ADDED: Base Logic for Game Tic Tac Toe

--- COMP392_MidTerm_PB/main.cpp
+++ COMP392_MidTerm_PB/main.cpp
@@ -1,7 +1,18 @@
 #include <iostream>
+#include <string>
+#include "TicTacToe.h"
 
 int main()
 {
 	std::cout << "Plaban Test...";
+
+	Game::TicTacToe game;
+
+	game.playGame();
+
+	std::cout << "\nThank You for Playing!!";
+	system("PAUSE>null");
+
+	return 0;
 	return 0;
 }
